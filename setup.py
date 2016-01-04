b2bba9eb00848450a0a38b6fdd64b0965ef0bb01
David Gee <david.gee@brocade.com>
2016-01-04 17:25:42 +0000

    Corrected setup.py message

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup for clicrud
 from setuptools import setup, find_packages
 
 setup(name='clicrud',
-      version='0.1.4',
+      version='0.1.6',
       description='Brocade CLI CRUD Operations Library.',
       author='Brocade Communications Systems, Inc.',
       author_email='dgee@brocade.com',
